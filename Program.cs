5db97df42de9b3a65d74fdb9c9cbccbe83fb1665
Tim_Hechenberger/HTL <Tim-Hechenberger@users.noreply.github.com>
2024-09-13 11:05:40 +0200

    first commit

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DesignPattern_Iterator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
